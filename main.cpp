4d22eaf8d10f012fa2459c6b96aa335df877ff05
Hafiz Hamdani <89209617+hyderoo@users.noreply.github.com>
2022-03-26 14:15:32 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+HAFIZ HAMDANI 21120121140106
+CITRA MAHARANI 21120121140109
+MUHAMMAD FAQIH IMAADUDDIN 21120121110038
+FAIZ AGIL GUSTIANNO 21120121130077
+KELOMPOK 25
+SHIFT 2  */
+
+#include <iostream>
+using namespace std;
+#define pi 22/7
+
+int main()
+{
+    int d = 28;
+    float r, kell;
+    r = 0.5 * d;
+    kell = (0.75 * pi * d)+r+r;
+
+    cout << "===== Program Menghitung Keliling 3/4 Lingkaran =====\n";
+    cout << "Keliling 3/4 Lingkaran : " << kell <<endl;
+    cout << "=====================================================\n";
+    return 0;
+}
